9d59359ae51d97f95d36031aebee3950272c6b74
현성 김 <hs-kim@mantech.co.kr>
2022-10-23 22:01:42 +0900

    컴포넌트 스캔

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,5 +1,8 @@
 package hello.core;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberRepository;
@@ -9,21 +12,26 @@ import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
 
+@Configuration
 public class AppConfig {
 
+  @Bean
   public MemberService memberService() {
     return new MemberServiceImpl(memberRepository());
   }
 
-  private MemberRepository memberRepository() {
+  @Bean
+  public MemberRepository memberRepository() {
     return new MemoryMemberRepository();
   }
 
+  @Bean
   public OrderService orderService() {
     return new OrderServiceImpl(memberRepository(), discountPolicy());
   }
 
-  private DiscountPolicy discountPolicy() {
+  @Bean
+  public DiscountPolicy discountPolicy() {
 //    return new FixDiscountPolicy();
     return new RateDiscountPolicy();
   }
---
+++ src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -0,0 +1,18 @@
+package hello.core.singleton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import hello.core.AppConfig;
+
+public class ConfigurationSingletonTest {
+
+  @Test
+  void configurationDeep() {
+    ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+    AppConfig bean = ac.getBean(AppConfig.class);
+
+    System.out.println("bean = " + bean.getClass());
+  }
+}
